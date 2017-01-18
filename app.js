240abbc667c766bb3d6b384137f15f5de8100a96
Bryce Holland <bholland@performancegpa.com>
2017-01-18 11:38:27 -0600

    add createConnection instead of connect

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ var theport = process.env.PORT || 5000;
 
 // Makes connection asynchronously.  Mongoose will queue up database
 // operations and release them when the connection is complete.
-mongoose.connect(uristring, function (err, res) {
+mongoose.createConnection(uristring, function (err, res) {
   if (err) {
   console.log ('ERROR connecting to: ' + uristring + '. ' + err);
   } else {
