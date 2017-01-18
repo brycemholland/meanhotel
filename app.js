b64bb598c0f5bfac05e08f51485218eb5775a852
Bryce Holland <bholland@performancegpa.com>
2017-01-18 11:56:07 -0600

    try to keep working with heroku

--- app.js
+++ app.js
@@ -8,37 +8,16 @@ var mongoose = require('mongoose');
 
 var routes = require('./api/routes');
 
-// Here we find an appropriate database to connect to, defaulting to
-// localhost if we don't find one.
-var uristring =
-process.env.MONGODB_URI ||
-//process.env.MONGOHQ_URL ||
-'mongodb://localhost/meanhotel';
-
-// The http server will listen to an appropriate port, or default to
-// port 5000.
-//var theport = process.env.PORT || 5000;
-
-// Makes connection asynchronously.  Mongoose will queue up database
-// operations and release them when the connection is complete.
-mongoose.createConnection(uristring, function (err, res) {
-  if (err) {
-  console.log ('ERROR connecting to: ' + uristring + '. ' + err);
-  } else {
-  console.log ('Succeeded connected to: ' + uristring);
-  }
-});
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/mean-hotel');
 
 // Define the port to run on
-//app.set('port', 3030);
+app.set('port', 3030);
 
 // Add middleware to console log every request
 app.use(function(req, res, next) {
   console.log(req.method, req.url);
   next(); 
 });
-
-//mongoose.connect(process.env.MONGOLAB_URI || 'mongodb://localhost/mean-hotel');
 
 // Set static directory before defining routes
 app.use(express.static(path.join(__dirname, 'public')));
